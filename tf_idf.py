7309bb54349bf85c5637d4c4ee69e03ec8d3dfb9
Ronnypetson <rsronnypetson4@gmail.com>
2017-09-15 11:54:27 -0300

    Encapsulated tfidf functionalities

--- tf_idf.py
+++ tf_idf.py
@@ -1,46 +1,79 @@
+from __future__ import print_function
+import glob, os
 import numpy as np
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Hiperparameters: size of grams
+class Tf_idf_eval:
+    def __init__(self,text_dir):
+        self.text_dir = text_dir
+        self.txtList = []
+        self.pathList = []
+        self.tfidf = None
 
-def tf_idf(counts):
-    transformer = TfidfTransformer(smooth_idf=True)
-    tfidf = transformer.fit_transform(counts)
-    return tfidf.toarray()
+    def load_tfidf(self):
+        if self.tfidf != None:
+            return
+        
+        path = self.text_dir
+        os.chdir(path)
+        
+        import codecs
+        for fl in glob.glob('*.txt'):
+            self.pathList.append(fl)
+            with open(fl) as f:
+                self.txtList.append(f.read())
+        
+        corpus = np.asarray(self.txtList)
+        self.vectorizer = CountVectorizer(min_df=1)
+        bag = self.vectorizer.fit_transform(corpus)
+        transformer = TfidfTransformer(smooth_idf=True)
+        self.tfidf = transformer.fit_transform(bag)
 
-#corpus = [
-#    'This is the first document.',
-#    'This is the second second document.',
-#    'And the third one.',
-#    'Is this the first document?',
-#]
+    def get_tfidf(self,text):
+        if self.tfidf == None:
+            self.load_tfidf()
+        return self.vectorizer.transform([text]).toarray()
+    
+    def compare(self,text1,text2):
+        tf1 = self.get_tfidf(text1)
+        tf2 = self.get_tfidf(text2)
+        return cosine_similarity(tf1,tf2)[0][0]
 
-path = 'texts/got.txt'
+txt_dir = 'peticoes_tokenized/'
+test_text_1 = 'pagar debito acrescido custas processuais juros atualizacao monetaria'
+test_text_2 = 'pagar debito acrescido bananas processuais juros atualizacao monetaria'
+evaluator = Tf_idf_eval(txt_dir)
+## For testing
+#evaluator.load_tfidf()  # Can call 'get_tfidf' or 'compare' directly 
+#print(get_tfidf(test_text_1))
+##
+print(test_text_1)
+print(test_text_2)
+print("Similaridade: "+str(evaluator.compare(test_text_1,test_text_2)))
 
-import codecs
-data = codecs.open(path,encoding='ISO-8859-1').read().lower()
-
-txtList = data.split(".")
-corpus = np.asarray(txtList)
-# analyze = vectorizer.build_analyzer(), analyze("This is a text document to analyze.")
-# vectorizer.get_feature_names(), X.toarray(), vectorizer.vocabulary_.get('document')
-# vectorizer.transform(['Something completely new.']).toarray()
-# Bag of 2-grams also available
-
-vectorizer = CountVectorizer(min_df=1)
-vectorizer.fit_transform(corpus)
-
-def cos_sim(f1,f2):
-    c1 = vectorizer.transform([f1]).toarray()
-    c2 = vectorizer.transform([f2]).toarray()
-    tf1 = tf_idf(c1)
-    tf2 = tf_idf(c2)
-    return cosine_similarity(tf1,tf2)
-
-f1 = "the water pipe is off since friday"
-f2 = "No water since friday. We need a pumbler to fix the water pipe"
-print("Comparando:\n\n"+f1+"\n\n"+f2+"\n")
-print("Similaridade: "+str(cos_sim(f1,f2)[0][0]))
+## Scratch
+#os.chdir(path)
+#txtList = []
+#pathList = []
+#import codecs
+#for fl in glob.glob('*.txt'):
+#    pathList.append(fl)
+#    #print(fl)
+#    with open(fl) as f:
+#        txtList.append(f.read())
+#corpus = np.asarray(txtList)
+#vectorizer = CountVectorizer(min_df=1)
+#bag = vectorizer.fit_transform(corpus)
+#transformer = TfidfTransformer(smooth_idf=True)
+#tfidf = transformer.fit_transform(bag)
+#d1 = 3
+#d2 = 9
+#t1 = vectorizer.transform(['pagar debito acrescido custas processuais juros atualizacao monetaria']).toarray()
+#t2 = vectorizer.transform(['']).toarray()
+#t1 = transformer.transform(t1).toarray()
+#t2 = transformer.transform(t2).toarray()
+#print(t)
+##
 
